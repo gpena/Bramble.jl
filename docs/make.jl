6700b57bfad5d7d3f7ef9e0ba71e8bebe25039c1
Gonçalo Pena <goncalo.pena@uc.pt>
2024-09-16 15:46:02 +0100

    remove html file

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,8 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, Bramble
 
-makedocs(sitename="Bramble")
+makedocs(sitename="Bramble",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    )
+)
