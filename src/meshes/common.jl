89ecc7a8f1bf8afacf5ab94610ff6632441d155a
Gonçalo Pena <goncalo.pena@uc.pt>
2024-09-18 20:55:18 +0100

    Update common.jl
    fix docstrings

--- src/meshes/common.jl
+++ src/meshes/common.jl
@@ -1,41 +1,35 @@
+"""
+Abstract type for meshes of topological dimension `D`.
+"""
 abstract type MeshType{D} <: BrambleType end
 
 VecCartIndex{D} = Set{CartesianIndex{D}} where D
 MeshMarkers{D} = Dict{String,VecCartIndex{D}} where D
 
 """
-	ndofs(mesh::MeshType)
-
-Return the number of points in the mesh
+$(SIGNATURES)
+Returns the number of points in the `mesh`.
 """
-ndofs(mesh::MeshType) = mesh.npts
+npoints(mesh::MeshType) = mesh.npts
 
 """
-	dim(mesh::MeshType)
-
-Return the dimension of the mesh
+$(SIGNATURES)
+Returns the topological dimension of the `mesh`.
 """
 dim(mesh::MeshType{D}) where D = D
 
 """
-	eltype(mesh::MeshType)
-
-Return the type of the points in the mesh
+$(SIGNATURES)
+Returns the element type of the `mesh` points.
 """
 eltype(mesh::MeshType) = eltype(typeof(mesh))
 
 """
-	indices(mesh::MeshType)
-
-Return the indices of the points in the mesh
+$(SIGNATURES)
+Returns the `CartesianIndices` associated with the `mesh` points.
 """
 indices(mesh::MeshType{D}) where D = (mesh.indices)::CartesianIndices{D}
 
-"""
-	marker(mesh::MeshType, m)
-
-Return the indices of the points in the mesh that have the marker `m`
-"""
 marker(mesh::MeshType, m) = mesh.markers[m]
 
 @inline _index2point(pts, idx) = pts[idx]
