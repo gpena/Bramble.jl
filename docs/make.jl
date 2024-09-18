de8e84cc01ecbfcb2afa4ab36a837910f0c02722
Gonçalo Pena <goncalo.pena@uc.pt>
2024-09-18 15:50:40 +0100

    test new deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,16 @@
-push!(LOAD_PATH,joinpath(@__DIR__,".."))
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 
 using Documenter
 using Bramble
 
 include("pages.jl")
 
-makedocs(; sitename="Bramble.jl")
+makedocs(;
+		 modules = [Bramble],
+		 format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
+		 sitename = "Bramble.jl",
+		 pages = ["Home" => "index.md",
+			 "internals.md"],
+		 authors = "Gonçalo Pena")
 
-deploydocs(repo = "github.com/gpena/gpena.github.io.git")
+deploydocs(; repo = "github.com/gpena/Bramble.jl")
