10e99bc398850427e6efed90515298f051eb93c9
Gonçalo Pena <goncalo.pena@uc.pt>
2024-09-21 00:14:11 +0100

    Update runtests.jl
    add spaces testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,31 +4,35 @@ using LinearAlgebra: norm, \
 
 using Bramble
 
-
 #Aqua.test_all(Bramble)
 
 validate_zero(u) = norm(u, Inf) < 1e-9
-validate_equal(u,v) = validate_zero(u.-v)
+validate_equal(u, v) = validate_zero(u .- v)
 
 function leastsquares(x::AbstractVector, y::AbstractVector)
-    A = [sum(x .* x) sum(x);
-         sum(x) length(x)]
+	A = [sum(x .* x) sum(x);
+		 sum(x) length(x)]
 
-    b = [sum(x .* y); sum(y)]
+	b = [sum(x .* y); sum(y)]
 
-    res = A \ b
-    return res[1], res[2]
+	res = A \ b
+	return res[1], res[2]
 end
-
 
 sep = "######"
 
 @time @testset "\n$sep Sets and Domains $sep" begin
-    include("sets.jl")
-    include("domains.jl")
+	include("sets.jl")
+	include("domains.jl")
 end
 
 @time @testset "\n$sep Meshes $sep" begin
-  include("mesh1d.jl")
-  include("meshnd.jl")
+	include("mesh1d.jl")
+	include("meshnd.jl")
+end
+
+@time @testset "\n$sep Grid spaces $sep" begin
+	include("gridspaces.jl")
+	include("vectorelements.jl")
+	include("matrixelements.jl")
 end
