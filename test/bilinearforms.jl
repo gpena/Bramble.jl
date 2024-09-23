495f4c55dd8cf1ced930e0c632afa7887e517ac3
Gonçalo Pena <goncalo.pena@uc.pt>
2024-09-23 16:50:51 +0100

    Create bilinearforms.jl
    tests for bilinearforms

---
+++ test/bilinearforms.jl
@@ -0,0 +1,35 @@
+using Bramble
+
+function bilinearforms_test()
+    N = 3
+    I = interval(-1.0, 4.0)
+
+    X = domain(I, create_markers("dd" => x -> x - 4.0))
+    Mh = mesh(X, N, false)
+
+    Wh = gridspace(Mh)
+
+    a(U, V) = innerₕ(U, V)
+    bform = BilinearForm(a, Wh, Wh)
+    A = assemble(bform)
+
+    bform2 = BilinearForm((U, V) -> inner₊(D₋ₓ(U), D₋ₓ(V)), Wh, Wh)
+    assemble(bform2)
+
+    bform3 = BilinearForm((U, V) -> inner₊(Mₕ(U), D₋ₓ(V)), Wh, Wh)
+    assemble(bform3)
+    
+    gh = element(Wh)
+    Rₕ!(gh, x->sin(x[1]))
+
+    l(V) = innerₕ(gh, V)
+    lform = LinearForm(l, Wh)
+    F = assemble(lform)
+
+    u = solve(A, F)
+
+    @test validate_equal( u, sin.(points(Mh)))
+    
+end
+
+bilinearforms_test()
