e4e72357b3adbf049a9b1f39ac8c5cd67ea2b2e7
Gonçalo Pena <goncalo.pena@uc.pt>
2024-10-14 12:17:34 +0100

    Update linearforms.jl
    - add support for choice of assembly strategy for linear forms

--- src/forms/linearforms.jl
+++ src/forms/linearforms.jl
@@ -5,6 +5,11 @@ Abstract type for linear forms.
 """
 abstract type LinearFormType <: BrambleType end
 
+struct DefaultAssembly <: BrambleType end
+struct InPlaceAssembly <: BrambleType end
+struct OperatorsAssembly <: BrambleType end
+struct AutoDetect <: BrambleType end
+
 """
 	struct LinearForm{TestType,F} <: LinearFormType
 		test_space::TestType
@@ -22,9 +27,10 @@ l \\colon & W_h &\\longrightarrow &\\mathbb{R} \\\\
 
 The field `form_expr` has the expression of the form and the remaining field stores the test space ``W_h``.
 """
-struct LinearForm{TestType,F} <: LinearFormType
+struct LinearForm{TestType,F,AssemblyType} <: LinearFormType
 	test_space::TestType
 	form_expr::F
+	strategy::AssemblyType
 end
 
 """
@@ -39,36 +45,78 @@ testspace(l::LinearForm) = l.test_space
 
 Returns a linear form from a given expression and a test space.
 """
-form(Wₕ::SpaceType, f::F) where F = LinearForm{typeof(Wₕ),F}(Wₕ, f)
+@inline function form(Wₕ::SpaceType, _f::F; strategy = DefaultAssembly(), verbose::Bool = false) where F
+	available_strategy = []
+
+	try
+		#@show "test inplace"
+		z = IdentityOperator(Wₕ)
+		x = element(Wₕ)
+		_f(x, z)
+		#@show available_strategy
+		push!(available_strategy, InPlaceAssembly())
+		#@show available_strategy
+	catch
+		try
+			#@show "test operators"
+			z = IdentityOperator(Wₕ)
+			_f(z)
+			push!(available_strategy, OperatorsAssembly())
+		catch #e2
+			#@show e2
+		end
+	end
+	#@show available_strategy
+	if length(available_strategy) >= 1 && strategy isa AutoDetect
+		strat = available_strategy[end]
+		f = _form_expr2fwrapper(_f, Wₕ, strat)
+
+		if verbose
+			println("Linear form will be assembled using $(strat) strategy.")
+		end
+		return LinearForm{typeof(Wₕ),typeof(f),typeof(strat)}(Wₕ, f, strat)
+	end
+
+	f = _form_expr2fwrapper(_f, Wₕ, strategy)
+	
+	if verbose 
+		println("Linear form will be assembled using $(strategy) strategy.")
+	end
+	return LinearForm{typeof(Wₕ),typeof(f),typeof(strategy)}(Wₕ, f, strategy)
+end
+
+_form_expr2fwrapper(f, S, _::DefaultAssembly) = FunctionWrapper{Vector{eltype(S)},Tuple{typeof(elements(S))}}(f)
+_form_expr2fwrapper(f, S, _::OperatorsAssembly) = FunctionWrapper{Vector{eltype(S)},Tuple{typeof(IdentityOperator(S))}}(f)
+_form_expr2fwrapper(f, S, _::InPlaceAssembly) = FunctionWrapper{Nothing, Tuple{Vector{eltype(S)},typeof(IdentityOperator(S))}}(f)
 
 """
 	assemble(l::LinearForm)
 
 Returns the assembled linear form as a vector.
 """
-function assemble(l::LinearForm)
-	z = elements(testspace(l))
-
-	return l.form_expr(z)
-end
+@inline assemble(l::LinearForm) = _assemble(l, l.strategy)
+@inline _assemble(l::LinearForm, ::DefaultAssembly) = l.form_expr(elements(testspace(l)))
+@inline _assemble(l::LinearForm, ::OperatorsAssembly) = l.form_expr(IdentityOperator(testspace(l)))
+@inline _assemble(l::LinearForm, ::InPlaceAssembly) = @error "Please call `assemble!(x, l)` instead of `assemble(l)`"
+#@inline _assemble(l::LinearForm, ::AutoDetect) = @error "The linearform shouldn't have the AutoDetect strategy"
 
 """
 	assemble!(x::AbstractVector, l::LinearForm)
 
 In-place assemble of a linear form into a given vector.
 """
-function assemble!(x::AbstractVector, l::LinearForm)
-	x .= l.form_expr(elements(testspace(l)))
-end
+@inline assemble!(x::AbstractVector, l::LinearForm) = _assemble!(x, l, l.strategy)
+
+@inline _assemble!(x::AbstractVector, l::LinearForm, ::DefaultAssembly) = x .= l.form_expr(elements(testspace(l)))
+@inline _assemble!(x::AbstractVector, l::LinearForm, ::OperatorsAssembly) = x .= l.form_expr(IdentityOperator(testspace(l)))
+@inline _assemble!(x::AbstractVector, l::LinearForm, ::InPlaceAssembly) = l.form_expr(x, IdentityOperator(testspace(l)))
 
 """
 	assemble!(x::VectorElement, l::LinearForm)
 
 In-place assemble of a linear form into a given [VectorElement](@ref).
 """
-function assemble!(x::VectorElement, l::LinearForm)
-	x .= l.form_expr(elements(testspace(l)))
-end
+@inline assemble!(x::VectorElement, l::LinearForm) = (assemble!(x.values, l))
 
 """
 	assemble(l::LinearForm, bcs::Constraints)
