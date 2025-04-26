5d1a0c973bda8f6e7f88e1436ce4000c33a06370
Gonçalo Pena <goncalo.pena@uc.pt>
2025-04-26 14:31:13 +0100

    Update aqua.jl

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,17 +1,13 @@
 using Aqua
-using Bramble
 
-@testset "\nAqua tests" begin
-	Aqua.test_all(
-	  Bramble;
-	  piracies=true,
-	  ambiguities = false,
-	  unbound_args = false,
-	  undefined_exports = false,
-	  project_extras = false,
-	  stale_deps = false,
-	  deps_compat = false,
-	  persistent_tasks = true
-	)
-  end
- 
+@testset "Aqua analysis" begin
+	Aqua.test_all(Bramble;
+				  piracies = true,
+				  ambiguities = false,
+				  unbound_args = false,
+				  undefined_exports = true,
+				  project_extras = false,
+				  stale_deps = false,
+				  deps_compat = false,
+				  persistent_tasks = true)
+end
