65b91dd85f9ac9247947d86bcc62a5f41326514f
Gonçalo Pena <goncalo.pena@uc.pt>
2025-05-01 14:59:51 +0100

    Update jet.jl

--- test/jet.jl
+++ test/jet.jl
@@ -3,7 +3,7 @@ using StyledStrings
 using OhMyREPL
 
 @testset "JET Static Analysis" begin
-	jet_report = JET.report_package(Bramble, ignored_modules = (Base, StyledStrings, OhMyREPL))
+	jet_report = JET.report_package(Bramble, ignored_modules = (Base, StyledStrings, OhMyREPL), analyze_from_definitions = true, toplevel_logger = nothing)
 
 	reports = JET.get_reports(jet_report)
 	@test length(reports) == 0
