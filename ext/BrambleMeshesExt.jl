4bafd6f558de4fe6d571f662017de4c0e0d2b42d
Gonçalo Pena <goncalo.pena@uc.pt>
2025-05-01 16:01:45 +0100

    Update BrambleMeshesExt.jl
    add viz for sets and meshes

--- ext/BrambleMeshesExt.jl
+++ ext/BrambleMeshesExt.jl
@@ -1,27 +1,47 @@
 module BrambleMeshesExt
-using Bramble: CartesianProduct
+using Bramble: CartesianProduct, dim, Mesh1D, MeshnD, points
 
-if !isdefined(Base, :get_extension)
-	using ..Meshes, ..GLMakie
-else
-	using Meshes
-	using GLMakie
-end
+using Meshes: Meshes, RectilinearGrid
 
+@inline _error_install_makie() = println("Please install `GLMakie` and add `using GLMakie` to your preamble.")
 """
 	Meshes.viz(X::CartesianProduct{D,T}) where {D,T}
 
 Visualize a [CartesianProduct](@ref).
 """
-function Meshes.viz(X::CartesianProduct{D,T}) where {D,T}
+function Meshes.viz(X::CartesianProduct)
+	D = dim(X)
 	@assert D >= 2
-	println("Meshes")
-	println(X.box)
+
 	mins = ntuple(i -> X.box[i][1], D)
 	maxs = ntuple(i -> X.box[i][2], D)
 	mesh_set = Meshes.Box(mins, maxs)
-	println(mesh_set)
-	Meshes.viz(mesh_set)
+
+	try
+		Meshes.viz(mesh_set)
+	catch
+		_error_install_makie()
+	end
 end
+
+"""
+	Meshes.viz(Ωₕ::MeshType)
+
+Visualize a [MeshnD](@ref).
+"""
+function Meshes.viz(M::MeshnD{D}) where D
+	@assert D >= 2
+
+	pts = points(M)
+	grid = Meshes.RectilinearGrid(pts...)
+
+	try
+		Meshes.viz(grid, showsegments = true)
+	catch
+		_error_install_makie()
+	end
+end
+
+Meshes.viz(M::Mesh1D) = @error "Visualization of 1D meshes is not supported"
 
 end
