33a8553b806f212f5e38bc4cfdbfcc0c16687602
Gonçalo Pena <goncalo.pena@uc.pt>
2025-05-02 11:04:32 +0100

    remove last line

--- test/jet.jl
+++ test/jet.jl
@@ -2,7 +2,7 @@ using JET
 using StyledStrings
 using OhMyREPL
 
-@testset "JET Static Analysis" begin
+@testset "JET static analysis" begin
 	jet_report = JET.report_package(Bramble, ignored_modules = (Base, StyledStrings, OhMyREPL), analyze_from_definitions = true, toplevel_logger = nothing)
 
 	reports = JET.get_reports(jet_report)
