243aaa2566614dcfe0d8b3df5544667eafab3333
Gonçalo Pena <goncalo.pena@uc.pt>
2025-05-09 19:49:37 +0100

    Update jet.jl
    remove StyledStrings

--- test/jet.jl
+++ test/jet.jl
@@ -1,10 +1,9 @@
 using JET
-using StyledStrings
 using OhMyREPL
 using Bramble
 
 @testset "JET static analysis" begin
-	jet_report = JET.report_package(Bramble, ignored_modules = (Base, StyledStrings, OhMyREPL), analyze_from_definitions = true, toplevel_logger = nothing)
+	jet_report = JET.report_package(Bramble, ignored_modules = (Base, OhMyREPL), analyze_from_definitions = true, toplevel_logger = nothing)
 
 	reports = JET.get_reports(jet_report)
 	@test length(reports) == 0
