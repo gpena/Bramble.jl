4b63afb2ae27bbe21d619032d33a214b52f8314a
Gonçalo Pena <goncalo.pena@uc.pt>
2025-10-20 11:26:36 +0100

    Add packages and functions
    - add MacroTools,
    - add functions related with sparse matrices
    - export dim and npoints

--- src/Bramble.jl
+++ src/Bramble.jl
@@ -6,107 +6,107 @@ using Base: remove_linenums!
 
 import Base: eltype, similar, length, copyto!, axes, materialize!
 import Base: show, first, last, getindex, setindex!, iterate, size, ndims, firstindex, lastindex
-#import Base: map, map!, first, last
-#import Base: *, +, -, /, ^
+import Base: *, +, -, /, ^, @propagate_inbounds, @_inline_meta
+
 using Random: rand!
 
-using SparseArrays: SparseMatrixCSC#, AbstractSparseMatrix, spdiagm
+using SparseArrays: SparseMatrixCSC, AbstractSparseMatrix, spdiagm, rowvals, nnz, dropzeros!, nzrange, spzeros, nonzeros
 
 using FunctionWrappers: FunctionWrapper
-using Lazy: @forward
+using MacroTools: @capture, isexpr
 
 using StaticArrays
 
 using OrderedCollections: LittleDict, OrderedDict, freeze
-#using OhMyThreads: tforeach, tmap!, @tasks
 using Base.Threads: @threads
 using UnPack: @unpack
 using MuladdMacro
 
-using LinearAlgebra: norm#Diagonal, mul!, I
-#=import LinearAlgebra: ⋅
+using LinearAlgebra: norm, Diagonal, mul!, I, dot, transpose, Transpose
+import LinearAlgebra: ⋅
 
 using FillArrays: Ones, Eye
-=#
+
 using Cubature
-using Integrals: solve, IntegralFunction, IntegralProblem, QuadGKJL, CubatureJLh
+using Integrals: solve, IntegralFunction, IntegralProblem, QuadGKJL, CubatureJLh, BatchIntegralFunction
 #using WriteVTK
 
 abstract type BrambleType end
 
 # domain/interval handling functions
 export box, interval, ×
-export domain, markers, labels
+export domain, markers, labels, dim
 
 # Mesh handling
-export mesh, hₘₐₓ, iterative_refinement!, change_points!
+export mesh, hₘₐₓ, iterative_refinement!, change_points, npoints
 
 # Space handling
 export gridspace, element
 
 export Rₕ, Rₕ!, avgₕ, avgₕ!
 export ndofs
-#=
-export innerₕ, innerₕ!
+
+export innerₕ
 export inner₊, inner₊ₓ, inner₊ᵧ, inner₊₂
 export snorm₁ₕ, norm₁ₕ, norm₊, normₕ
 
 export diff₋ₓ, diff₋ᵧ, diff₋₂, diff₋ₕ
+export diff₊ₓ, diff₊ᵧ, diff₊₂, diff₊ₕ
+
 export D₋ₓ, D₋ᵧ, D₋₂, ∇₋ₕ
-export diffₓ, diffᵧ, diff₂, diffₕ
-export jumpₓ, jumpᵧ, jump₂, jumpₕ
-export M₋ₕₓ, M₋ₕᵧ, M₋ₕ₂, M₋ₕ
+export D₊ₓ, D₊ᵧ, D₊₂, ∇₊ₕ
+
+export jump₋ₓ, jump₋ᵧ, jump₋₂, jump₋ₕ
+export jump₊ₓ, jump₊ᵧ, jump₊₂, jump₊ₕ
+
+export M₋ₓ, M₋ᵧ, M₋₂, M₋ₕ
+export M₊ₓ, M₊ᵧ, M₊₂, M₊ₕ
 
 export ⋅
 
-# Forms exports
-export AutoDetect, DefaultAssembly, InPlaceAssembly, OperatorsAssembly
-export form
-export assemble, assemble!
-export constraints, symmetrize!
-=#
+export dirichlet_constraints
+export form, assemble, assemble!
+
 #=
 # Exporters
 export ExporterVTK, addScalarDataset!, datasets, save2file, close
 =#
 
-#include("utils/style.jl")
+include("utils/macros.jl")
 include("utils/backend.jl")
-include("utils/linearalgebra.jl")
+include("utils/linear_algebra.jl")
 
-include("geometry/sets.jl")
-include("utils/bramblefunction.jl")
+include("geometry/set.jl")
+include("utils/bramble_function.jl")
 
-include("geometry/markers.jl")
-include("geometry/domains.jl")
+include("geometry/marker.jl")
+include("geometry/domain.jl")
 
-include("meshes/common.jl")
-include("meshes/markers.jl")
-include("meshes/mesh1d.jl")
-include("meshes/meshnd.jl")
+include("mesh/common.jl")
+include("mesh/marker.jl")
+include("mesh/mesh1d.jl")
+include("mesh/meshnd.jl")
 
-include("spaces/buffer.jl")
-include("spaces/types.jl")
-include("spaces/gridspace.jl")
-include("spaces/vectorelements.jl")
+include("space/buffer.jl")
+include("space/gridspace.jl")
+include("space/scalar_gridspace.jl")
+include("space/vector_gridspace.jl")
+include("space/vectorelement.jl")
 
-#=include("spaces/matrixelements.jl")
-include("spaces/difference_utils.jl")
-include("spaces/backward_difference.jl")
-include("spaces/backward_finite_difference.jl")
-include("spaces/forward_difference.jl")
-include("spaces/jump.jl")
-include("spaces/average.jl")
-include("spaces/inner_product.jl")
-include("spaces/operators.jl")
+include("space/matrixelement.jl")
+include("space/operators/shift.jl")
+include("space/operators/difference.jl")
+include("space/operators/jump.jl")
+include("space/operators/average.jl")
+include("space/inner_product.jl")
 
-include("forms/constraints.jl")
-include("forms/bilinearforms.jl")
-include("forms/linearforms.jl")
-=#
+include("form/dirichlet_constraints.jl")
+include("form/bilinear_form.jl")
+include("form/linear_form.jl")
+
 #=
-include("exporters/types.jl")
-include("exporters/exporter_vtk.jl")
+include("exporter/types.jl")
+include("exporter/exporter_vtk.jl")
 
 =#
 #include("precompile.jl")
