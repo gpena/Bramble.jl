fe7a86d9f8a3f2cf0fd9b5e482558dfc81e8084a
Gonçalo Pena <goncalo.pena@uc.pt>
2025-10-28 14:42:39 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,11 @@ using Documenter
 #using LiveServer; servedocs()
 
 home = "Home" => "index.md"
-internals = "Internals" => ["internals/utils.md", "internals/geometry.md", "internals/mesh.md",#="internals/space.md", "internals/forms.md"=#]
-#examples = "Examples" => ["examples/poisson_linear.md", "examples/poisson_nonlinear.md"]
+internals = "Internals" => ["internals/utils.md", "internals/geometry.md", "internals/mesh.md", "internals/space.md", "internals/form.md"]
+examples = "Examples" => ["examples/poisson_linear.md", "examples/poisson_nonlinear.md"]
 documentation = "Documentation" => ["api.md", internals]
 allpages = [home,
-	#=examples,=#
+	examples,
 	documentation]
 
 makedocs(; format = Documenter.HTML(),
