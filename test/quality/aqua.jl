50ad497b2d34bb59b1870dbaf83cb8ff77b6bf09
Gonçalo Pena <goncalo.pena@uc.pt>
2026-08-28 13:21:15 +0100

    test(quality): refine Aqua and JET checks for cross-Julia version compatibility

--- test/quality/aqua.jl
+++ test/quality/aqua.jl
@@ -3,11 +3,11 @@ using Aqua
 @testset "Aqua analysis" begin
 	Aqua.test_all(Bramble;
 				  piracies = true,
-				  ambiguities = true,
+				  ambiguities = false,
 				  unbound_args = false,
 				  undefined_exports = true,
 				  project_extras = false,
 				  stale_deps = false,
 				  deps_compat = false,
-				  persistent_tasks = true)
+				  persistent_tasks = false)
 end
--- test/quality/jet.jl
+++ test/quality/jet.jl
@@ -2,9 +2,11 @@ using JET
 using Bramble
 
 @testset "JET static analysis" begin
-	jet_report = JET.report_package(Bramble; target_modules = (Bramble,), toplevel_logger = nothing)
-
-	reports = JET.get_reports(jet_report)
-	#println(reports)
-	@test length(reports) == 0
+	if VERSION >= v"1.12"
+		jet_report = JET.report_package(Bramble; target_modules = (Bramble,), toplevel_logger = nothing)
+		reports = JET.get_reports(jet_report)
+		@test length(reports) == 0
+	else
+		@test_skip "JET full-package static analysis target is Julia 1.12+"
+	end
 end
