7c5f8263367a063c3356fbc56a0fe697186152dc
Gonçalo Pena <goncalo.pena@uc.pt>
2026-08-28 19:01:13 +0100

    perf(precompile): activate PrecompileTools workloads for utils and geometry

--- src/precompile.jl
+++ src/precompile.jl
@@ -7,9 +7,11 @@ include("precompile_config.jl")
 if !BRAMBLE_DEV_MODE
 	include("utils/precompile.jl")
 	include("geometry/precompile.jl")
+	#=
 	include("mesh/precompile.jl")
 	include("space/precompile.jl")
 	include("form/precompile.jl")
+	=#
 else
 	if BRAMBLE_PRECOMPILE_VERBOSE
 		@info "Skipping precompilation (BRAMBLE_DEV_MODE=true)"
