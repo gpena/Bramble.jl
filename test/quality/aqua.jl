d5800f65e590b2f58bc61a58de21b9d4da71486c
Gonçalo Pena <goncalo.pena@uc.pt>
2026-08-28 18:56:11 +0100

    test(aqua): configure Aqua to strictly check code quality (ambiguities, unbound args, exports, piracy, tasks)

--- test/quality/aqua.jl
+++ test/quality/aqua.jl
@@ -3,12 +3,12 @@ using Aqua
 @testset "Aqua analysis" begin
 	Aqua.test_all(Bramble;
 				  piracies = true,
-				  ambiguities = false,
+				  ambiguities = true,
 				  unbound_args = true,
 				  undefined_exports = true,
 				  project_extras = false,
 				  stale_deps = false,
 				  deps_compat = false,
-				  persistent_tasks = false)
+				  persistent_tasks = true)
 	Aqua.test_ambiguities(Bramble; recursive = false)
 end
